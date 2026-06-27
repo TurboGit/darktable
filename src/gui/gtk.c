fbd79c8282ac9b0b009a6cd38f7abea3ec117f14
Pascal Obry <pascal@obry.net>
2026-06-27 12:19:31 +0200

    Introduce generic theme variants.
    The main theme can now be adapted with variants.
     - condensed
     - rounded header
     - module border (light for focused module, dark for expanded ones)
     - colored accent (green, blue, yellow, orange)
    
    The "condensed" variant was already available into the
    Miscellaneous / Interface section. It is now part of the general
    theme section and handled as a standard variant.
    
    The variants controls are proposed just below the main theme and are
    indented to make it clear that those are part of the theming.

--- src/gui/gtk.c
+++ src/gui/gtk.c
@@ -3709,6 +3709,42 @@ void dt_gui_load_theme(const char *theme)
     _add_theme_import(&themecss, datadir, "themes", "chunk-condensed.css");
   }
 
+  // chunk-rounded-header
+
+  if(dt_conf_get_bool("themes/rounded-header"))
+  {
+    _add_theme_import(&themecss, datadir, "themes", "chunk-rounded-header.css");
+  }
+
+  // chunk-rounded-header
+
+  const int index = dt_conf_get_int("themes/colored-accent");
+
+  if(index > 0)
+  {
+    const char *colors[] =
+        {
+            "none",
+            "green",
+            "blue",
+            "yellow",
+            "orange"
+        };
+
+    char *chunk = g_strdup_printf
+      ("chunk-%s-colored-accent.css",
+       colors[index]);
+    _add_theme_import(&themecss, datadir, "themes", chunk);
+    g_free(chunk);
+  }
+
+  // chunk-module-border
+
+  if(dt_conf_get_bool("themes/module-border"))
+  {
+    _add_theme_import(&themecss, datadir, "themes", "chunk-module-border.css");
+  }
+
   // load any OS specific themes tweak file to fix some platform specific issues
 
 #ifdef __APPLE__
